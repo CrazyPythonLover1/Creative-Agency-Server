1e3882bd48815e320fdc854d18eac844f4247766
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-14 01:20:21 +0600

    connect mongodb and cors , body-parser

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgaci.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
 const app = express()
-const port = 3000
+const port = 5000
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db(process.env.DB_NAME).collection("services");
+  
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
