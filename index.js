b7a0eb33d3860beb68895061e3f226dada93fabb
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-16 23:49:40 +0600

    deploy heroku

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.connect(err => {
   const placeOrderCollection = client.db("Creative-Agency").collection("placeOrder");
   const reviewCollection = client.db("Creative-Agency").collection("reviews");
   const adminCollection = client.db("Creative-Agency").collection("adminList");
-  
+
 
   app.post('/addService', (req, res) => {
     const file = req.files.file;
@@ -38,8 +38,8 @@ client.connect(err => {
     const filePath = `${__dirname}/services/${file.name}`;
 
     file.mv(filePath, err => {
-      if(err){
-        res.status(500).send({message: "Failed to upload image"});
+      if (err) {
+        res.status(500).send({ message: "Failed to upload image" });
       }
 
       const newImg = fs.readFileSync(filePath);
@@ -51,28 +51,28 @@ client.connect(err => {
         img: Buffer(encImg, 'base64')
       }
 
-      serviceCollection.insertOne({title, description, image})
-      .then(result => {
-        fs.remove(filePath, error => {
-          if(error){
-            console.log(error);
-            res.status(500).send({message: "Failed to upload Image "});
-          }
-          res.send(result.insertedCount > 0)
+      serviceCollection.insertOne({ title, description, image })
+        .then(result => {
+          fs.remove(filePath, error => {
+            if (error) {
+              console.log(error);
+              res.status(500).send({ message: "Failed to upload Image " });
+            }
+            res.send(result.insertedCount > 0)
+          })
         })
-      })
 
-      return res.send({name: file.name, path: `/${file.name}`})
+      return res.send({ name: file.name, path: `/${file.name}` })
 
     })
 
   })
 
-  app.get('/getServices', (req,res) => {
+  app.get('/getServices', (req, res) => {
     serviceCollection.find({})
-    .toArray((err, docs)=>{
-      res.status(200).send(docs);
-    })
+      .toArray((err, docs) => {
+        res.status(200).send(docs);
+      })
   })
 
 
@@ -85,90 +85,82 @@ client.connect(err => {
     const description = req.body.description;
     const price = req.body.price;
     const serviceId = req.body.serviceId;
-    const filePath = `${__dirname}/placeOrder/${file.name}`;
+   
 
-    file.mv(filePath, err => {
-      if(err){
-        res.status(500).send({message: "Failed to upload image"});
-      }
 
-      const newImg = fs.readFileSync(filePath);
-      const encImg = newImg.toString('base64');
+    const newImg = file.data;
+    const encImg = newImg.toString('base64');
 
-      var image = {
-        contentType: req.files.file.mimeType,
-        size: req.files.file.size,
-        img: Buffer(encImg, 'base64')
-      }
+    var image = {
+      contentType: file.mimeType,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
+    }
 
-      placeOrderCollection.insertOne({name, email, selectedServiceName, description, price, image, serviceId})
+    placeOrderCollection.insertOne({ name, email, selectedServiceName, description, price, image, serviceId })
       .then(result => {
-        fs.remove(filePath, error => {
-          if(error){
-            console.log(error);
-            res.status(500).send({message: "Failed to upload Image "});
-          }
-          res.send(result.insertedCount > 0)
-        })
+
+        res.send(result.insertedCount > 0)
       })
 
-      return res.send({name: file.name, path: `/${file.name}`})
+    return res.send({ name: file.name, path: `/${file.name}` })
 
-    })
 
   })
 
 
-  app.post('/getUserOrderList', (req,res) => {
+  app.post('/getUserOrderList', (req, res) => {
     const email = req.body.email;
-    placeOrderCollection.find({email: email})
-    .toArray((err, docs)=>{
-      res.status(200).send(docs);
-    })
+    placeOrderCollection.find({ email: email })
+      .toArray((err, docs) => {
+        res.status(200).send(docs);
+      })
   })
 
-  app.post('/getOrderedServiceList', (req,res) => {
+  app.post('/getOrderedServiceList', (req, res) => {
     const serviceId = req.body.serviceId;
     console.log(serviceId)
-    serviceCollection.find({_id: ObjectId(serviceId)})
-      .toArray((err, docs)=>{
-      res.status(200).send(docs);
-    
+    serviceCollection.find({ _id: ObjectId(serviceId) })
+      .toArray((err, docs) => {
+        res.status(200).send(docs);
+
+      })
+
+  });
+
+  app.post('/addReview', (req, res) => {
+    const review = req.body;
+    reviewCollection.insertOne(review)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+
   })
 
-});
-
-app.post('/addReview', (req, res)=> {
-  const review  = req.body;
-  reviewCollection.insertOne(review)
-  .then(result => {
-    res.send(result.insertedCount > 0 )
+  app.get('/getRviews', (req, res) => {
+    reviewCollection.find({}).limit(6)
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
   })
 
-})
-
-app.get('/getRviews', (req,res)=> {
-  reviewCollection.find({}).limit(6)
-  .toArray((err,documents)=> {
-    res.send(documents);
+  app.get('/all-order-list', (req, res) => {
+    placeOrderCollection.find({})
+      .toArray((err, documents) => {
+        res.status(200).send(documents)
+      })
   })
-})
 
-app.get('/all-order-list', (req,res)=> {
-  placeOrderCollection.find({})
-  .toArray((err, documents)=> {
-    res.status(200).send(documents)
+
+  app.post('/addAdmin', (req, res) => {
+    const email = req.body;
+    adminCollection.insertOne(email)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   })
-})
 
 
-app.post('/addAdmin', (req,res) => {
-  const email = req.body;
-  adminCollection.insertOne(email)
-  .then(result =>  {
-    res.send(result.insertedCount > 0 )
-  })
-})
 
 
 
@@ -176,6 +168,6 @@ app.post('/addAdmin', (req,res) => {
 
 // const PORT = process.env.PORT || 5000
 const port = 5000
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
